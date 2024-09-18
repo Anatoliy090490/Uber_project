5de7e17e5d0e43bb39d9f4adeb2fd5b980891950
Anatoliy <zevsinin@gmail.com>
2024-09-18 16:26:56 +0300

    git add -A

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,10 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(rename({
+            suffix: '.min', 
+            prefix: ''
+        }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
